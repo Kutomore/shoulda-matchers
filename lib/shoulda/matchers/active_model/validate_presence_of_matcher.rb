d5b2f8c20b87b5f0eb18d7ff777629766f97ee24
Lee Machin <me@mrl.ee>
2018-07-20 19:07:49 +0100

    Check that InstanceMethodsOnActivation is specifically defined
    Rails 6 no longer has such a module

--- lib/shoulda/matchers/active_model/validate_presence_of_matcher.rb
+++ lib/shoulda/matchers/active_model/validate_presence_of_matcher.rb
@@ -147,9 +147,17 @@ module Shoulda
         private
 
         def secure_password_being_validated?
-          defined?(::ActiveModel::SecurePassword) &&
-            @subject.class.ancestors.include?(::ActiveModel::SecurePassword::InstanceMethodsOnActivation) &&
+          if defined?(::ActiveModel::SecurePassword::InstanceMethodsOnActivation)
+            return false unless @subject.class.ancestors.include?(::ActiveModel::SecurePassword::InstanceMethodsOnActivation)
+
             @attribute == :password
+          else
+            auth_attr = @subject.class.instance_methods.find {|meth| meth.to_s =~ /authenticate_[\w_]+/ }
+
+            return false unless auth_attr
+
+            @attribute = auth_attr.delete('authenticate_')
+          end
         end
 
         def possibly_ignore_interference_by_writer
